4ad53569c89a983bf00b24f4ffe61d7dc1194992
Eddishot <Eddi_sl94@hotmail.it>
2021-10-07 16:09:04 +0200

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+
+
+
+const nomeUtente = prompt("Qual'è il tuo nome?");
+
+const cognomeUtente = prompt("Qual'è il tuo cognome?");
+
+console.log("check")
+
+const colorePreferito = prompt("Qual'è il tuo colore preferito?");
+
+alert(`${nomeUtente} ${cognomeUtente} ${colorePreferito} 21.`);
